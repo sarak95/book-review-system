19ac5d08bbcaaaed8513fb7c67a9a5c9d24b33c4
sarak95 <sarakonjokrad95@gmail.com>
2025-02-22 12:49:22 +0100

    added User, Book, Author and Tag model with migrations

---
+++ app/Models/Book.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'publication_year', 'description', 'author_id'];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
