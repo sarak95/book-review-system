285919300d923501789657e0e2d4f77e161dc533
sarak95 <sarakonjokrad95@gmail.com>
2025-02-22 16:27:46 +0100

    added AdminUserSeeder seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+    }
+}
