f6cf00296949f730325c7ad64bf84d50d19e9d1a
sarak95 <sarakonjokrad95@gmail.com>
2025-02-22 16:21:22 +0100

    added BookRequest, AuthorRequest and TagRequest

---
+++ app/Http/Requests/AuthorRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthorRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true; 
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'bio'  => 'nullable|string',
+        ];
+    }
+}
